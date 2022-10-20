7d9e1a38c380a3fd1d64308da42af0c7cd855a60
Kuba99Reniec <110913509+Kuba99Reniec@users.noreply.github.com>
2022-10-20 18:28:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,164 +1,90 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+f!pip install dash
+!pip install pandas
 import dash
-from dash.dependencies import Input, Output
-from dash import dash_table, dcc, html
-from dash.dash_table.Format import Format
-import plotly.express as px
-
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
 import pandas as pd
 
-# read data file
-# ------------------------------------------------------------------------------
-df = pd.read_csv(
-    "nba_physiques.csv",
-    index_col=0,
-    dtype={"Year": "int"}
-)
-# manage data
-df = df.assign(bmi=df.weight / ((df.height / 100) ** 2))
-df = df.assign(height_bins=pd.qcut(df.height, q=4))
-df = df[["Year", "height", "weight", "bmi",
-         "PER", "PTS", "pos_simple", "height_bins"]]
+dane_cawi_capi = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/dane_do_dasha.txt")
+options = []
+for col in dane_cawi_capi.columns[:-1]:
+    options.append({'label':'{}'.format(col, col), 'value':col})
+value = 'Wielkość gospodarstwa domowego (6 kategorii)'
+zmienne = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/5_opis_zmiennych_20220819_podwojny_konwerter_opinie.txt", sep="\t")
+wartosci = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/6.txt", sep="\t", decimal =",")
+wartosci = wartosci.merge(zmienne, on = "Nazwa zmiennej")
+zmienna = 'RS: Płeć'
 
-
-# Set up app
-# ------------------------------------------------------------------------------
-external_stylesheets = [
-    'https://codepen.io/chriddyp/pen/bWLwgP.css',
-]
-
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-
-# HTML page Layout
-# ------------------------------------------------------------------------------
-# Page is divided in three parts:
-#    * header: at the top, a title
-#    * body: the main containt
-#    * footer: at the bottom, contact, informations, credits
 app.layout = html.Div([
-    # ------ header
-    html.Div(
-        className="header",
-        style={"backgroundColor": "#3c6382"},
-        children=[html.H2(
-            "Stats on NBA players - Dash app example",
-            style={
-                "color": "white",
-                "padding": "30px 0 30px 0",
-                "textAlign": "center"}
-        )],
-    ),
-
-    # ----- body
-    html.Div(className="container", children=[
-        # a sub title
-        html.H3("A plot"),
-        # first dropdown selector
-        dcc.Dropdown(
-            id="x-dropdown",  # identifiant
-            value="height",  # default value
-            # all values in the menu
-            options=[{"label": name, "value": name} for name in df.columns],
+    html.H3('Porównanie rozkładów zmiennych: CAWI vs CAWI + CAPI (IX 2021 - VIII 2022)'),
+    html.Label("Wybierz zmienną:"),
+    dcc.Dropdown(
+            id = 'my_dropdown',
+            options= options,
+            value='Choose columns'
         ),
-        # second dropdown selector
-        dcc.Dropdown(
-            id="y-dropdown",
-            value="weight",
-            options=[{"label": name, "value": name} for name in df.columns],
-        ),
-        # a place for the plot with an id
-        html.Div(
-            dcc.Graph(id='graph'),
-        ),
-        # a line
-        html.Hr(),
-        # a sub title
-        html.H3("A table"),
-        # a new dropdown
-        dcc.Dropdown(
-            id="pivot-dropdown",
-            value="height",
-            options=[
-                {"label": name, "value": name} 
-                for name in ["Year", "height", "weight", "bmi", "PER", "PTS"]
-            ],
-        ),
-        # a table for data
-        dash_table.DataTable(
-            id="pivot-table",
-        ),
-    ]),
-
-    # ----- footer
-    html.Div(
-        className="footer",
-        style={"backgroundColor": "#3c6382"},
-        children=[html.H2(
-            "https://github.com/gVallverdu/dash-example-NBA",
-            style={
-                "color": "white",
-                "padding": "30px 0 30px 0",
-                "textAlign": "center"}
-        )],
-    ),
+    html.Label(id='my_label'),
+    dcc.Graph(
+        id='graph',
+        figure=go.Figure()
+    )
 ])
-
-# Callback functions => interactivity
-# Each element on the page is identified thanks to its `id`
-# ------------------------------------------------------------------------------
-
-
 @app.callback(
     Output('graph', 'figure'),
-    [Input("x-dropdown", "value"),
-     Input("y-dropdown", "value")],
+    Input('my_dropdown', 'value'),
+    State('graph', 'figure'),
 )
-def display_graph(xvalue, yvalue):
-    """ 
-    This function produce the plot.
-
-    The output is the "figure" of the graph
-    The inputs, are the values of the two dropdown menus
-    """
-
-    figure = px.scatter(
-        df,
-        x=xvalue, y=yvalue,
-        color='pos_simple',
-        category_orders=dict(pos_simple=['PG', 'SG', 'SF', 'PF', 'C']),
-        marginal_x="histogram",
-        marginal_y="histogram",
-        template="plotly_white",
+def update_figure(value, fig):
+    zmienna = value
+    df_pom = pd.DataFrame(dane_cawi_capi[dane_cawi_capi['badanie'] == 'cawi'][zmienna].value_counts())
+    names_cawi_pom = df_pom.index.to_list()
+    values_cawi = df_pom[zmienna].to_list()
+    values_cawi_capi = []
+    names_cawi = []
+    for i in names_cawi_pom:
+        values_cawi_capi.append(len(dane_cawi_capi[dane_cawi_capi[zmienna] == i]))
+        if zmienna in wartosci['Etykieta zmiennej'].to_list():
+            names_cawi.append(
+                wartosci[(wartosci['Etykieta zmiennej'] == zmienna) & (wartosci['wartość'] == i)]['Etykieta'].to_list()[
+                    0])
+        else:
+            names_cawi = names_cawi_pom
+    fig = go.Figure()
+    fig.add_trace(go.Funnelarea(
+        name="CAWI",
+        scalegroup="first",
+        text=names_cawi,
+        values=values_cawi,
+        showlegend=False,
+        marker={"colors": ["lawngreen", "darkorange", "red", "yellow", "blue", "peru", "darkviolet"]},
+        title={"position": "top center", "text": "CAWI"},
+    ))
+    fig.add_trace(go.Funnelarea(
+        name="CAWI + CAPI",
+        scalegroup="third",
+        text=names_cawi,
+        values=values_cawi_capi,
+        textinfo="percent",
+        title={"position": "top center", "text": "CAWI+CAPI"},
+        showlegend=False,
+        domain={"x": [.75, 1]}
+    ))
+    fig.update_layout(
+        autosize=False,
+        width=1150,
+        height=600)
+    fig.update_traces(title_font_size=20)
+    fig.update_traces(title_position='top center')
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=20, b=25),
     )
-
-    return figure
-
-
-@app.callback(
-    [Output("pivot-table", "data"),
-     Output("pivot-table", "columns")],
-    [Input("pivot-dropdown", "value")]
-)
-def show_pivot_table(value):
-    """ This function return a pivot Table """
-
-    pivot_df = pd.pivot_table(
-        data=df, values=value, columns="pos_simple", index="height_bins"
-    )
-    pivot_df = pivot_df.reset_index()
-    pivot_df = pivot_df.astype({"height_bins": "str"})
-    cols = [{
-        "name": col, 
-        "id": col,
-        "type": "numeric",
-        "format": Format(precision=5)
-    } for col in pivot_df.columns]
-    data = pivot_df.to_dict("records")
-
-    return data, cols
+    return fig
 
 
 if __name__ == '__main__':
