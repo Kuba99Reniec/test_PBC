2aa212c01c486d2cc9d4649b79ca73a318ad11ae
Kuba99Reniec <110913509+Kuba99Reniec@users.noreply.github.com>
2022-10-25 16:28:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 import dash
+from dash import dash_table
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
 import pandas as pd
-dane_cawi_capi = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/dane_do_dasha.txt", sep="\t")
+import numpy as np
+dane_cawi_capi = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/dane_do_dasha_cpw.txt", sep="\t")
 options = []
-for col in dane_cawi_capi.columns[:-1]:
+tytuly = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/test_PBC/main/tytuly.txt", sep="\t")
+tytuly = tytuly[tytuly['zmienna']!='BUDDOM']
+tytuly = tytuly[tytuly['zmienna']!='CZASWNE']
+tytuly = tytuly[tytuly['zmienna']!='KROPTV']
+tytuly = tytuly[tytuly['zmienna']!='OCZLEK']
+tytuly = tytuly[tytuly['zmienna']!='VOGUE']
+tytuly = tytuly.reset_index(drop = True)
+cpw = pd.DataFrame()
+dane_cawi = dane_cawi_capi[dane_cawi_capi['badanie'] == 'cawi']
+for i in range(len(tytuly)):
+    cpw.loc[tytuly.loc[i, 'tytul'], 'CPW oryginalne'] = round(
+                np.average(dane_cawi_capi[tytuly.loc[i, 'zmienna'] + '5'], weights=dane_cawi_capi['WAGAOSOB']), 2)
+    cpw.loc[tytuly.loc[i, 'tytul'], 'CPW oryginalne po przejściu na CAWI'] = round(
+                np.average(dane_cawi[tytuly.loc[i, 'zmienna'] + '5'], weights=dane_cawi['WAGAOSOB']), 2)
+    cpw.loc[tytuly.loc[i, 'tytul'], 'CPW do raportu czytelnictwa'] = round(
+                np.average(dane_cawi_capi[tytuly.loc[i, 'zmienna'] + '5_2C'], weights=dane_cawi_capi['WAGAOSOB']),
+                2)
+    cpw.loc[tytuly.loc[i, 'tytul'], 'CPW do mediaplanu'] = round(
+                np.average(dane_cawi_capi[tytuly.loc[i, 'zmienna'] + '5_24a'], weights=dane_cawi_capi['WAGAOSOB']),
+                2)
+cpw = cpw.reset_index(drop=False)
+cpw = cpw.rename(columns={'index': 'pismo'})
+cpw[''] = cpw['pismo']
+cpw = cpw.set_index('')
+options_2 = sorted(list(set(tytuly['grupa tematyczna'].to_list())))
+for col in dane_cawi_capi.columns[:87]:
     options.append({'label':'{}'.format(col, col), 'value':col})
 value = 'Wielkość gospodarstwa domowego (6 kategorii)'
 zmienne = pd.read_csv("https://raw.githubusercontent.com/Kuba99Reniec/wykresy/master/5_opis_zmiennych_20220819_podwojny_konwerter_opinie.txt", sep="\t")
@@ -38,7 +65,37 @@ app.layout = html.Div([
         style={'width': '49%', 'display': 'inline-block', 'height': '82%'}),
     dcc.Graph(
         id='graph_2',
-        style={'width': '49%', 'align': 'right', 'display': 'inline-block', 'height': '82%'})
+        style={'width': '49%', 'align': 'right', 'display': 'inline-block', 'height': '82%'}),
+    html.Label("Wybierz grupę tematyczną:"),
+    dcc.Dropdown(
+            id = 'table_dropdown',
+            options=options_2,
+            value='kobiece: exclusive'
+        ),
+    dash_table.DataTable(
+        id='dynamic_table',
+        columns=[{'name': col, 'id': col} for col in cpw.columns],
+        data=cpw.to_dict('records'),
+        style_data={
+        'color': 'black',
+        'backgroundColor': 'white'},
+        style_data_conditional=[
+        {
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(220, 220, 220)',
+        }],
+        style_cell_conditional=[
+            {'if': {'column_id': ''},
+             'width': '16%', 'textAlign': 'left'},
+            {'if': {'column_id': 'CPW oryginalne'},
+             'width': '21%', 'textAlign': 'center'},
+            {'if': {'column_id': 'CPW oryginalne po przejściu na CAWI'},
+             'width': '21%', 'textAlign': 'center'},
+            {'if': {'column_id': 'CPW do raportu czytelnictwa'},
+             'width': '21%', 'textAlign': 'center'},
+            {'if': {'column_id': 'CPW do mediaplanu'},
+             'width': '21%', 'textAlign': 'center'}
+        ])
 ])
 @app.callback(
     Output('graph', 'figure'),
@@ -72,7 +129,7 @@ def update_figure(value, fig):
     fig.update_traces(title_font_size=20)
     fig.update_traces(title_position='top center')
     fig.update_layout(margin=dict(t=0, b=30, l=0, r=0))
-    fig.update_traces(aspectratio=0.6)
+    fig.update_traces(aspectratio=0.5)
     return fig
 @app.callback(
     Output('graph_2', 'figure'),
@@ -105,7 +162,11 @@ def update_figure(value, fig):
     fig.update_traces(title_font_size=20)
     fig.update_traces(title_position='top center')
     fig.update_layout(margin=dict(t=0, b=30, l=0, r=0))
-    fig.update_traces(aspectratio=0.6)
+    fig.update_traces(aspectratio=0.5)
     return fig
+@app.callback(Output('dynamic_table','data'),
+             [Input('table_dropdown','value')],)
+def get_corresponding_rows(value):
+    return cpw.loc[sorted(list(set(tytuly['tytul'][tytuly['grupa tematyczna']==value].to_list()))),].to_dict('records')
 if __name__ == '__main__':
     app.run_server(debug=True)
